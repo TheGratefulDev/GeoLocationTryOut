831f8212bc539c3a26a36368b47a3c01d6d2da2b
Ka Ling <ka.ling@live.com>
2020-06-26 20:38:49 -0500

    make collection and document id easier

--- Where/app/src/main/java/coffee/ka/where/Constant.java
+++ Where/app/src/main/java/coffee/ka/where/Constant.java
@@ -2,8 +2,8 @@ package coffee.ka.where;
 
 class Constant {
 
-	static final String COLLECTION_NAME = "orders";
-	static final String DOCUMENT_ID = "insert_your_document_id_here";
+	static final String COLLECTION_NAME = "demo";
+	static final String DOCUMENT_ID = "sample";
 	static final String FIELDS_NAME = "latlng";
 
 }
